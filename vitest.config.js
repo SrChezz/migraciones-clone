a3fecf00057fb731c46e505311ae6d0ef41249b8
Chezzby <itzchezvil17@gmail.com>
2024-11-18 04:23:43 -0500

    v0 ready

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,6 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+import { fileURLToPath } from 'node:url';
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
+import viteConfig from './vite.config';
 
 export default mergeConfig(
   viteConfig,
@@ -10,5 +10,10 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
-  }),
-)
+    base: '/migraciones-clone/',
+    plugins: [],
+    resolve: {
+      alias: [],
+    },
+  })
+);
