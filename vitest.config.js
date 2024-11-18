a8c704c3e0d8ca1e1e14181935f2b0dc009f294b
Chezzby <itzchezvil17@gmail.com>
2024-11-18 04:54:44 -0500

    test 12312

--- vitest.config.js
+++ vitest.config.js
@@ -2,14 +2,10 @@ import { fileURLToPath } from 'node:url';
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
 import viteConfig from './vite.config';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-    },
-    base: 'https://SrChezz.github.io/migraciones-clone/',
-  })
-);
+export default defineConfig({
+  base: '/migraciones-clone/',
+  plugins: [],
+  resolve: {
+    alias: [],
+  },
+});
